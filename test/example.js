dd6382e8a7f61f5527375f311763630ceca64aa6
Ademar Arvati Filho <aarvati@hotmail.com>
2019-06-07 10:25:00 -0300

    crypto sign and keys
    Signed-off-by: Ademar Arvati Filho <aarvati@hotmail.com>

--- test/example.js
+++ test/example.js
@@ -1,4 +1,3 @@
-const send = require('micro').send;
 const microParseRequest = require('../lib/');
 const {getKeyPair, querySearch, pathMatch, sign, verify} = microParseRequest.utils
 
@@ -9,7 +8,7 @@ const options ={
   path: ['/micro/param1/:id1/(.*)','/micro/param2/:id2/(.*)'],
   param:{}
 }
-options.keyPair = getKeyPair(options.secret, 1024)
+options.keyPair = getKeyPair(options.secret, {type:'ec', namedCurve:'P-256K'})
 
 const parseRequest = microParseRequest(options)
 
